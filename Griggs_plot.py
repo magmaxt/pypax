39d780aa96addb6a580296e6db24fe6b35e19422
Xiaochuan Tian <x.tian@columbia.edu>
2024-12-05 17:48:14 -0800

    update plots

--- Griggs_plot.py
+++ Griggs_plot.py
@@ -90,7 +90,8 @@ for i in range(nod):
 
     # -------- COMPUTE AND PROCESS THE MESH DATA --------
 
-    sample_mask = sample.data > 0.9
+    sample_mask = sample.data > 0.90
+    #sample_mask = sample_mask * np.logical_and(sample.x > 0, sample.x < 0.0015)
     sample_mask = sample_mask * np.logical_and(sample.x > 0, sample.x < 0.0015)
 
     sample_purity = sample.copy()
@@ -104,13 +105,42 @@ for i in range(nod):
 
 # -------- FIGURE --------
 # flow rule theoretical parameters:
+A = 1e-23 ##1e-2 MPa^-n/s is equivalent to 1e-23 Pa^-n/s for n=3.5#
+n = 3.5
+Q = 310000 # J/mol
+R = 8.314 # J/(mol K)
 
+T = 1273 # K
+stress_theory = np.logspace(0,20,num=21,base=10,endpoint=True) 
+strainrate_theory = A * stress_theory**n * np.exp(-Q/(R*T)) 
+
+strainrate_theory_newtonian = A * stress_theory**1 * np.exp(-Q/(R*T))  * 5e20
+#eta = A**(-1/n) * np.exp(Q/(n*R*T)) * epsilon**((1-n)/n)    /2 # * 1e6
+
+#mean
+mean_stress=np.mean(stressII_masked.data)
+mean_strainrate=np.mean(strainrateII_masked.data)
 
 fig = plt.figure()
 ax  = fig.add_subplot(111)
-ax.set_title('model: '+exp_name)
-cmap = ax.scatter(stressII_masked.data,strainrateII_masked.data,c=sample_purity.data,s=2,cmap=plt.cm.magma,zorder=1)
-ax.scatter(np.mean(stressII_masked.data),np.mean(strainrateII_masked.data),s=100,color='red',alpha=1,label='mean')
+ax.set_title(
+    f'model: {exp_name} \n @ {int(model_time[0] / sec_in_day)} days and strain of {strain_theory[0]:.1f}\n'
+    + f'mean strain rate: {mean_strainrate:.2e} ' + r'$\mathrm{s^{-1}}$' + f', mean stress: {mean_stress:.2e} Pa'
+)
+
+# theory plot dislocation n=3.5
+ax.loglog(stress_theory,strainrate_theory,'r--',label='dislocation creep from Zhang et al., 2006')
+# theory plot newtonian n=1
+ax.loglog(stress_theory,strainrate_theory_newtonian,'b--',label='diffusion creep')
+
+cmap = ax.scatter(stressII_masked.data,strainrateII_masked.data,c=sample_purity.data,s=33,cmap=plt.cm.magma,zorder=2)
+ax.scatter(mean_stress,mean_strainrate,s=100,color='red',alpha=1,label='mean')
+
+
+
+
+
+
 cbar = fig.colorbar(cmap,orientation='vertical',shrink=0.5,label='sample purity')
 ax.set_xscale('log')
 ax.set_yscale('log')
@@ -118,6 +148,21 @@ ax.set_xlabel(r'$\sigma_{II}^s$ (Pa)')
 ax.set_ylabel(r'$\epsilon_{II}^s (s^{-1})$')
 ax.legend(fontsize=9)
 fig.tight_layout()
+
+
+
+
+# plt.xlim([np.mean(stressII_masked.data)*0.1,np.mean(stressII_masked.data)*10])
+# plt.ylim([np.mean(strainrateII_masked.data)*0.1,np.mean(strainrateII_masked.data)*10])
+
+plt.xlim([1e8,1e10])
+plt.ylim([1e-7,1e-5])
+
+# # more relaxed
+# plt.xlim([1e7,1e11])
+# plt.ylim([1e-8,1e-4])
+plt.grid()
+
 plt.show()
 
 
